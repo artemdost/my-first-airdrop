61cdab4cce371ff0e73c30d7447cfa1667633da0
artemdost <dartem925@gmail.com>
2025-01-26 17:32:03 +0300

    last

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Jersey_15 } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,8 +11,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-const jersey = Jersey_15({ weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
